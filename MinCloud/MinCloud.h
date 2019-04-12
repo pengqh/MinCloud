e50205ec1acedd16f7c1147cbd3c362e2b8c1526
pqhifanr <pengquanhua@ifanr.com>
2019-04-12 10:00:42 +0800

    Initial Commit

---
+++ MinCloud/MinCloud.h
@@ -0,0 +1,19 @@
+//
+//  MinCloud.h
+//  MinCloud
+//
+//  Created by pengquanhua on 2019/4/12.
+//  Copyright © 2019 ifanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MinCloud.
+FOUNDATION_EXPORT double MinCloudVersionNumber;
+
+//! Project version string for MinCloud.
+FOUNDATION_EXPORT const unsigned char MinCloudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MinCloud/PublicHeader.h>
+
+
